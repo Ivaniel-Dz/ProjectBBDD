fa38ebae274ddc6c4964d44360b83addd56a6725
Ivaniel-Dz <pcivaniel@gmail.com>
2023-07-04 00:27:43 -0500

    Actulizar proyecto base

--- header.js
+++ header.js
@@ -1,7 +1,21 @@
+// const allSideMenu = document.querySelectorAll('sidebar .side-menu li a');
 
-$(".menu-icon").click(function () {
-$(this).toggleClass("active");
-$(".navigation-menu").toggleClass("active");
-$(".menu-icon i").toggleClass("fa-times"); 
-});
+// allSideMenu.forEach(item => {
+//     const li = item.parentElement;
 
+//     item.addEventListener('click', function() {
+//         allSideMenu.forEach( i=> {
+//             i.parentElement.classList.remove
+//         })
+//         li.classList.add('active');
+//     });
+// });
+
+
+
+
+
+
+if(window.innerWidth < 768){
+    sidebar.classList.add('hide');
+}
